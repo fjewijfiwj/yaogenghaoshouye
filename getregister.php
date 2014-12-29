6ffc0e244bd04608732b64ecb6a651256a474ec3
Huxiao@Thinkpad_Win8.1 <future.huxiao@hotmail.com>
2014-12-29 14:26:36 +0800

    home0.2.2
    完成注册信息mysql对接

--- getregister.php
+++ getregister.php
@@ -1,12 +1,31 @@
 <?php
 header("Content-Type: text/html; charset=UTF-8");
-header("Refresh:5;url=index.html");
+//header("Refresh:5;url=index.html");
 $school=$_POST["school"];
 $name=$_POST["name"];
 $phone=$_POST["phone"];
 $email=$_POST["email"];
+$time=date('Y-m-d H:i:s');
+$mysql = new SaeMysql();
 
-echo $school.$name.$phone.$email;
+
+//$sql = "INSERT  INTO `register` ( `school`,`name`,`phone`,`email`,`time`) VALUES ('1','2','2','1',,'989') ";
+//$sql = "INSERT  INTO `register` ( `school`) VALUES ($school) ";
+$sql="insert into register(school,name,phone,email,time) values('".$school."','".$name."','".$phone."','".$email."','".$time."')";
+
+$mysql->runSql($sql);
+if ($mysql->errno() != 0)
+{
+    die("Error:" . $mysql->errmsg());
+}else{
+    echo "恭喜您注册成功！\n 工作人员会在一个工作日与您取得联系。非常感谢！";
+
+}
+
+$mysql->closeDb();
+
+
+
 
 print('正在加载，请稍等...<br>五秒后自动跳转。');
 
