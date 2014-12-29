09cd8037c41cbef76f4c3b4a8b05fd0269686d3d
Huxiao@Thinkpad_Win8.1 <future.huxiao@hotmail.com>
2014-12-29 12:22:57 +0800

    home 0.1
    支持js 不同浏览器间跳转

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source "https://ruby.taobao.org"
+
+gem "sass", "~> 3.4.0"
+gem "compass", "~> 1.0"
+
---
+++ getregister.php
@@ -0,0 +1,8 @@
+<?php
+$school=$_POST["school"];
+$name=$_POST["name"];
+$phone=$_POST["phone"];
+$email=$_POST["email"];
+
+echo $school.$name.$phone.$email;
+?>
