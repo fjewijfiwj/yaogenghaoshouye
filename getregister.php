278e29d1453ab3396ca79a0229f0012e16252c5f
Huxiao@Thinkpad_Win8.1 <future.huxiao@hotmail.com>
2014-12-29 13:15:55 +0800

    home 0.2.1
    优化了form表单与后台，但是没有与SAE数据库打通

--- getregister.php
+++ getregister.php
@@ -1,8 +1,13 @@
 <?php
+header("Content-Type: text/html; charset=UTF-8");
+header("Refresh:5;url=index.html");
 $school=$_POST["school"];
 $name=$_POST["name"];
 $phone=$_POST["phone"];
 $email=$_POST["email"];
 
 echo $school.$name.$phone.$email;
+
+print('正在加载，请稍等...<br>五秒后自动跳转。');
+
 ?>
