419f0e615e20ba7574ddeaa984464414e44e557c
Huxiao@Thinkpad_Win8.1 <future.huxiao@hotmail.com>
2014-12-30 11:03:40 +0800

    home 0.2.3.1
    为注册加入了密码域

--- getregister.php
+++ getregister.php
@@ -1,24 +1,25 @@
 <?php
 header("Content-Type: text/html; charset=UTF-8");
-header("Refresh:5;url=index.html");
+header("Refresh:5;url=http://test.yaogenghao.com");
 $school=$_POST["school"];
 $name=$_POST["name"];
 $phone=$_POST["phone"];
 $email=$_POST["email"];
 $time=date('Y-m-d H:i:s');
+$password=$_POST["password"];
+$password2=$_POST["password2"];
 $mysql = new SaeMysql();
 
 
-//$sql = "INSERT  INTO `register` ( `school`,`name`,`phone`,`email`,`time`) VALUES ('1','2','2','1',,'989') ";
-//$sql = "INSERT  INTO `register` ( `school`) VALUES ($school) ";
-$sql="insert into register(school,name,phone,email,time) values('".$school."','".$name."','".$phone."','".$email."','".$time."')";
+
+$sql="insert into register(school,name,phone,email,time,password,password2) values('".$school."','".$name."','".$phone."','".$email."','".$time."','".$password."','".$password2."')";
 
 $mysql->runSql($sql);
 if ($mysql->errno() != 0)
 {
     die("Error:" . $mysql->errmsg());
 }else{
-    echo "恭喜您注册成功！\n 工作人员会在一个工作日与您取得联系。非常感谢！";
+    echo "恭喜您注册成功！\n 稍后请进行【系统偏好设置】";
 
 }
 
